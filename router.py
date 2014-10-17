99fb0506cf087ea2ff86b346ef442b10b8ee9573
mtacer <mtacer@rigdata.com>
2014-10-17 16:35:17 -0500

    reporting added to the parent LegalDescription class, storing to ORM implemented in the parent LegalDescription class

--- router.py
+++ router.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Table, MetaData, Column, Integer, Numeric, String, create_engine, ForeignKey
+import datetime
+from sqlalchemy import Table, MetaData, Column, Integer, Numeric, String, Date, create_engine, ForeignKey
 from sqlalchemy.orm import sessionmaker, mapper, relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-from models import Texas, Ohio_Virginia, Kentucky_Tennessee, NewYork, WV_Pensylvania, PLS, Canada_dls, Canada_ts
+from models import Texas, Ohio_Virginia, Kentucky_Tennessee, NewYork, WV_Pensylvania, PLS, British_Columbia_dls, British_Columbia_ts, Alberta_ts, Alberta_Saskatchewan_dls, Manitoba_dls
 
 Base = declarative_base()
 
@@ -95,6 +96,7 @@ class WellBore(Base):
     __tablename__ = 'WellBore'
 
     id  = Column('WellBoreID', Integer, primary_key=True)
+    well_no = Column('WellNumber', String)
     api = Column('API', String)
     uwi = Column('UWI', String)
 
@@ -104,14 +106,26 @@ class WellBore(Base):
 class WellBoreDetail(Base):
     __tablename__ = 'WellBoreDetail'
 
-    id          = Column('WellBoreDetailID', Integer, primary_key=True)
+    id                 = Column('WellBoreDetailID', Integer, primary_key=True)
+    wellbore_id        = Column('WellBoreID', Integer, ForeignKey(WellBore.id))
+    wellpoint_type_id  = Column('WellPointTypeID', Integer)
+    geo_id             = Column('GeographyID', Integer, ForeignKey(Geography.id))
+    locnum             = Column('Locnum', String)
+
+
+class Permit(Base):
+    __tablename__ = 'Permit'
+
+    id  = Column('PermitID', Integer, primary_key=True)
     wellbore_id = Column('WellBoreID', Integer, ForeignKey(WellBore.id))
-    geo_id      = Column('GeographyID', Integer, ForeignKey(Geography.id))
-    locnum      = Column('Locnum', String)
+    posted_date = Column('PermitPostedDate', Date)
 
 
-# defining regions where different coordinate calculation logic applies
 class Region(object):
+    """
+    defining regions where different coordinate calculation logic applies
+
+    """
     def __init__(self, name, criteria, model):
         self.name = name
         self.criteria = criteria
@@ -128,13 +142,14 @@ class Region(object):
 
 
 class Rec(object):
-    def __init__(self, coo, geo, cty, wbd, wb, sta):
+    def __init__(self, coo, geo, cty, wbd, wb, sta, prm):
         self.coo = coo
         self.geo = geo
         self.cty = cty
         self.wbd = wbd
         self.wb  = wb
         self.sta = sta
+        self.prm = prm
 
 
 def define_type(rec):
@@ -180,16 +195,36 @@ def define_type(rec):
          }),
 
         Region(**{
-            'name'      : 'canada_dls',
-            'criteria'  : rec.wb.uwi.startswith('1'),
-            'model'     : Canada_dls(rec)
+            'name'      : 'british_columbia_dls',
+            'criteria'  : rec.sta.state_code.upper() == 'BC' and rec.wb.uwi.startswith('1'),
+            'model'     : British_Columbia_dls(rec)
          }),
 
         Region(**{
-            'name'      : 'canada_ts',
-            'criteria'  : rec.wb.uwi.startswith('2'),
-            'model'     : Canada_ts(rec)
-         }),    ]
+            'name'      : 'british_columbia_ts',
+            'criteria'  : rec.sta.state_code.upper() == 'BC' and rec.wb.uwi.startswith('2'),
+            'model'     : British_Columbia_ts(rec)
+         }),    
+
+        # there are no known topographic survey records for Alberta
+        # Region(**{
+        #     'name'      : 'alberta_ts',
+        #     'criteria'  : rec.sta.state_code.upper() == 'AB' and rec.wb.uwi.startswith('2'),
+        #     'model'     : Alberta_ts(rec)
+        #  }),    
+
+        Region(**{
+            'name'      : 'alberta_saskatchewan_dls',
+            'criteria'  : (rec.sta.state_code.upper() == 'AB' and rec.wb.uwi.startswith('1') or rec.sta.state_code.upper() == 'SK'),
+            'model'     : Alberta_Saskatchewan_dls(rec)
+         }),    
+
+        Region(**{
+            'name'      : 'manitoba_dls',
+            'criteria'  : rec.sta.state_code.upper() == 'MB',
+            'model'     : Manitoba_dls(rec)
+         }),    
+    ]
 
 
     # if criteria is fulfilled return the above Type object, including the reference to a model
@@ -198,31 +233,50 @@ def define_type(rec):
             return region.get_model()
 
 
-
 if __name__ == '__main__':
     session = rigdata21_session_maker()
-    query = session.query(Geography, Coordinates, WellBoreDetail, WellBore, State, County)
-    # query = query.join(Geography, County, WellBoreDetail, WellBore).filter(WellBoreDetail.locnum == '2')
-    # query = query.join(Geography, County, WellBoreDetail, WellBore).filter(County.state_code == 'TX', WellBoreDetail.locnum == '1262686')
-    # query = query.join(Coordinates, County, WellBoreDetail, WellBore).filter(County.state_code == 'ND', WellBoreDetail.locnum == '771330')
-    # query = query.join(Coordinates, WellBoreDetail, WellBore, State).filter(State.state_code == 'AB', WellBoreDetail.locnum == '939708')
-    query = query.join(Coordinates, WellBoreDetail, WellBore, State).filter(State.state_code == 'AB', WellBoreDetail.locnum == '888480')
-    query = query.outerjoin(County)
-    # query = query.join(Geography, County, WellBoreDetail, WellBore)
+    query = session.query(Geography, Coordinates, WellBoreDetail, WellBore, State, County, Permit)
+    query = query.join(Coordinates, WellBoreDetail, WellBore, State).outerjoin(County).outerjoin(Permit)
+
+    query = query.filter(State.state_code == 'ND', Permit.posted_date < datetime.datetime(year=2011, month=1, day=1), Permit.posted_date > datetime.datetime(year=2008, month=1, day=1), WellBoreDetail.wellpoint_type_id == 513)
+    # query = query.filter(State.state_code == 'TX', WellBoreDetail.locnum == '775568')
+    # query = query.filter(State.state_code == 'TX', WellBoreDetail.wellpoint_type_id == 513)
+    # query = query.filter(State.state_code == 'SK', Permit.posted_date < datetime.datetime(year=2012, month=1, day=1), Permit.posted_date > datetime.datetime(year=2008, month=1, day=1), WellBore.uwi.startswith('2'))
     # print query
 
-    for i, (geo, coo, wbd, wb, sta, cty) in enumerate(query.limit(10), start=1):    
-    # for i, (coo, geo, cty, wbd, wb) in enumerate(query, start=1):    
-        rec = Rec(coo, geo, cty, wbd, wb, sta)
+    j = 0 #number of reported cases
+    for i, (geo, coo, wbd, wb, sta, cty, prm) in enumerate(query.limit(10), start=1):    
+    # for i, (geo, coo, wbd, wb, sta, cty, prm) in enumerate(query, start=1):    
+
+        # if not i % 100: print '%i records processed.' % i
+        rec = Rec(coo, geo, cty, wbd, wb, sta, prm)
         model_object = define_type(rec)
         if model_object:
-            pass
+
+            # calculate coordinates
             model_object.coordinates()
-            model_object.location_quality()
+
+            # find all the referenced shapes in the legal description
+            model_object.assign_ref_shapes()
+
+            # assign centroid, if  unable to calculate coordinates
             if not model_object.get_point():
                 model_object.assign_centroid()
-        else:
-            print rec.wbd.locnum, rec.coo.northing, rec.coo.easting, rec.coo.epsg_code, rec.geo.legal_desc, rec.sta.state_code
 
+            # assess the location quality
+            model_object.define_location_quality()
+            if model_object.get_location_quality() < 999:
+                j +=1
+                model_object.report()
+
+            # store point and location QA into the ORM (still need to commit  in order to persist changes in the database)
+            model_object.store_calculated_point_and_QA()
+        else:
+            print 'No model found for record for %s, API: %s, UWI: %s' % (rec.sta.state_code, rec.wb.api, rec.wb.uwi)
+            print '\t', rec.wbd.locnum, rec.coo.northing, rec.coo.easting, rec.coo.epsg_code, rec.geo.legal_desc
+
+    print '%i total records reported.' % j
+    print '%i total records processed.' % i
     # commits the changes into the database
     # session.commit()
+
