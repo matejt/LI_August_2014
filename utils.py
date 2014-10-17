12c5c5cd5d1e4a6ddae151dc64023a9be793fb18
mtacer <mtacer@rigdata.com>
2014-10-17 16:36:16 -0500

    rules modified to take care of Texas four letter direcions

--- utils.py
+++ utils.py
@@ -10,9 +10,20 @@ from pyparsing import commaSeparatedList
 conn_read  = pymssql.connect(host='RDSQLDEV\\RDSQLDEV')
 cur_read = conn_read.cursor()
 
-class CalcPointException(Exception): pass
 
-def ensure_polygon(geom_object):
+class UnitsException(Exception): pass
+
+
+class DirectionException(Exception): pass
+
+
+def ensure_polygon (geom_object):
+    
+    """
+        converts MultiPolygon to Polygon if necessary
+
+    """
+
     if geom_object.geom_type == 'Polygon':
         return geom_object
     elif geom_object.geom_type == 'MultiPolygon':
@@ -26,7 +37,7 @@ def ensure_polygon(geom_object):
         raise Exception('ensure_polygon function: unsupported geometry type: %s' % geom_object.geom_type)
 
 
-def meridian_zone(state_code, county_name, twnshp, twnshp_dir, range_, range_dir):
+def meridian_zone (state_code, county_name, twnshp, twnshp_dir, range_, range_dir):
 
     # Alabama state
     if state_code == 'AL':
@@ -99,7 +110,7 @@ def extend_line (line_string, distance):
     return LineString([p3, p4])
 
 
-def transform(epsg_in, epsg_out, point_in):
+def transform (epsg_in, epsg_out, point_in):
     # define source and destination coordinate systems based on the ESPG code
     srcProj = pyproj.Proj(init='epsg:%i' % epsg_in, preserve_units=True)
     dstProj = pyproj.Proj(init='epsg:%i' % epsg_out, preserve_units=True)
@@ -110,66 +121,94 @@ def transform(epsg_in, epsg_out, point_in):
     return point_out
 
 
-def ft2m(feet):
+def ft2m (feet):
     return feet * 0.3048
 
 
-def rules(dir1, dir2, lines):
+def rules (_dir, four_points):
 
-    if dir1 in ('FNL', 'FSL', 'FWL', 'FEL') and dir2 in ('FNL', 'FSL', 'FWL', 'FEL'):
-        return {'FNL': lines['north'], 'FSL': lines['south'], 'FEL': lines['east'], 'FWL': lines['west']}
+    centroid_wm = Point(avg(pnt.x for pnt in four_points), avg(pnt.y for pnt in four_points))
+    north_points = [pnt for pnt in four_points if pnt.y >= centroid_wm.y]
+    south_points = [pnt for pnt in four_points if pnt.y < centroid_wm.y]
+    min_x = min(pnt.x for pnt in four_points)
+    max_x = max(pnt.x for pnt in four_points)
+    min_y = min(pnt.y for pnt in four_points)
+    max_y = max(pnt.y for pnt in four_points)
+    min_x_north = min(pnt.x for pnt in north_points)
+    max_x_north = max(pnt.x for pnt in north_points)
+    min_x_south = min(pnt.x for pnt in south_points)
+    max_x_south = max(pnt.x for pnt in south_points)
 
-    if dir1 in ('FNEL', 'FSEL', 'FNWL', 'FSWL') and dir2 in ('FNEL', 'FSEL', 'FNWL', 'FSWL'):
-        return {'FNEL': lines['north'], 'FSWL': lines['south'], 'FSEL': lines['east'], 'FNWL': lines['west']}
+    if _dir in ('FNL', 'FSL', 'FEL', 'FWL'):
+        point_NE = [pnt for pnt in north_points if pnt.x == max_x_north ][0] # north-east point
+        point_NW = [pnt for pnt in north_points if pnt.x == min_x_north ][0] # north-west point
+        point_SE = [pnt for pnt in south_points if pnt.x == max_x_south ][0] # south-east point
+        point_SW = [pnt for pnt in south_points if pnt.x == min_x_south ][0] # south-west point
 
-    if dir1 in ('FNL', 'FSL', 'FNWL', 'FSEL') and dir2 in ('FNL', 'FSL', 'FNWL', 'FSEL'):
-        return {'FNL': lines['north'], 'FSL': lines['south'], 'FSEL': lines['east'], 'FNWL': lines['west']}
+        if _dir == 'FNL':
+            return LineString([point_NW, point_NE])
+        elif _dir =='FSL':
+            return LineString([point_SE, point_SW])
+        elif _dir =='FEL':
+            return LineString([point_NE, point_SE])
+        elif _dir =='FWL':
+            return LineString([point_SW, point_NW])
 
-    return None
+    elif _dir in ('FNWL', 'FNEL', 'FSWL', 'FSEL'):
+        point_N = [pnt for pnt in four_points if pnt.y == max_y ][0]
+        point_W = [pnt for pnt in four_points if pnt.x == min_x ][0]
+        point_S = [pnt for pnt in four_points if pnt.y == min_y ][0]
+        point_E = [pnt for pnt in four_points if pnt.x == max_x ][0]
+
+        if _dir == 'FNWL':
+            return LineString([point_W, point_N])
+        elif _dir =='FNEL':
+            return LineString([point_N, point_E])
+        elif _dir =='FSEL':
+            return LineString([point_E, point_S])
+        elif _dir =='FSWL':
+            return LineString([point_S, point_W])
+    else:
+        print '\tUnknown direction: %s' % _dir
+        # raise DirectionException('Unknown _direction: %s' % _dir)
+        return None
 
 
-def calc_point_from_offsets(four_corners_wm, dist1, dir1, dist2, dir2, units='feet'):
+def calc_point_from_offsets (four_corners_wm, dist1, dir1, dist2, dir2, units='feet'):
 
     if not units.lower() in ('feet', 'meters'):
-        raise CalcPointException('Unknown units: %s' % units)
+        raise UnitsException('Unknown units: %s' % units)
 
     dir1, dir2 = dir1.upper(), dir2.upper()
-    centroid_wm = Point(avg(pnt.x for pnt in four_corners_wm.values()), avg(pnt.y for pnt in four_corners_wm.values()))
-    # centroid_wm = four_corners_wm.centroid
+    centroid_wm = Point(avg(pnt.x for pnt in four_corners_wm), avg(pnt.y for pnt in four_corners_wm))
     centroid_nad83 = transform(3857, 4269, centroid_wm)
 
-    dist1, dist2 = float(dist1), float(dist2)
-
     wm_corr_factor = math.cos(centroid_nad83.y * math.pi / 180.0) # web mercator distance correction factor
-    # print 'wm_corr_factor: ', wm_corr_factor
-    
-    # create four LineStrings
-    lines = {
-        'north' : LineString([four_corners_wm['NW'], four_corners_wm['NE']]),
-        'east'  : LineString([four_corners_wm['NE'], four_corners_wm['SE']]),
-        'south' : LineString([four_corners_wm['SE'], four_corners_wm['SW']]),
-        'west'  : LineString([four_corners_wm['SW'], four_corners_wm['NW']]),
-    }
 
-    # lines = {'FNL': north_line, 'FSL': south_line, 'FEL': east_line, 'FWL': west_line, 'FSEL': east_line, 'FNEL': north_line}
-    lines = rules(dir1, dir2, lines)
-    if not lines: 
+    # for pnt in four_corners_wm:
+    #     print pnt
+
+    line1 = rules(dir1, four_corners_wm)
+    line2 = rules(dir2, four_corners_wm)
+
+    # print line1
+    # print line2
+
+    if not (all([line1, line2]) and all([line1.length, line2.length])): 
         print '\tcould not resolve offset lines.'
         return None
 
-    if units == 'feet':
-        offset_line1 = lines.get(dir1.upper(), None).parallel_offset(ft2m(dist1) / wm_corr_factor, 'right')
-        offset_line2 = lines.get(dir2.upper(), None).parallel_offset(ft2m(dist2) / wm_corr_factor, 'right')
-        # offset_line1 = lines.get(dir1, None).parallel_offset(ft2m(dist1), 'right')  # without the correction factor
-        # offset_line2 = lines.get(dir2, None).parallel_offset(ft2m(dist2), 'right')  # without the correction factor
-    elif units == 'meters':
-        offset_line1 = lines.get(dir1.upper(), None).parallel_offset(dist1 / wm_corr_factor, 'right')
-        offset_line2 = lines.get(dir2.upper(), None).parallel_offset(dist2 / wm_corr_factor, 'right')
-        # offset_line1 = lines.get(dir1, None).parallel_offset(ft2m(dist1), 'right')  # without the correction factor
-        # offset_line2 = lines.get(dir2, None).parallel_offset(ft2m(dist2), 'right')  # without the correction factor
+    # print 'd1: %.1f, d2: %.1f' % (dist1, dist2)
 
-    # for pnt in offset_line1.coords:
-    #     print pnt
+    if units == 'feet':
+        dist1 = ft2m(dist1)
+        dist2 = ft2m(dist2)
+
+    # print 'd1: %.1f, d2: %.1f' % (dist1, dist2)
+
+    # default web mercator units are meters
+    offset_line1 = line1.parallel_offset(dist1 / wm_corr_factor, 'right')
+    offset_line2 = line2.parallel_offset(dist2 / wm_corr_factor, 'right')
 
     pnt = offset_line1.intersection(offset_line2)
     if not pnt: 
@@ -186,11 +225,31 @@ def calc_point_from_offsets(four_corners_wm, dist1, dir1, dist2, dir2, units='fe
     return transform(3857, 4269, pnt)
 
 
-def bearing_deg(pnt1, pnt2):
+def area ( poly_geometry_wm, units='hectars' ):
+    """
+        returns area in hectars of the polygon geometry
+    """
+
+    if not units.lower() in ('hectars', 'square miles'):
+        raise UnitsException('Unknown units: %s' % units)
+
+    centroid_wm = poly_geometry_wm.centroid
+    centroid_nad83 = transform(3857, 4269, centroid_wm)
+
+    wm_corr_factor = math.cos(centroid_nad83.y * math.pi / 180.0) # web mercator distance correction factor
+    
+    area_ha = poly_geometry_wm.area * math.pow(wm_corr_factor, 2) / 10000.0
+    if units == 'hectars':
+        return area_ha
+    elif units == 'square miles':
+        return area_ha / 259.0
+
+
+def bearing_deg (pnt1, pnt2):
     return math.atan2(pnt2.x-pnt1.x, pnt2.y-pnt1.y) * 180.0 / math.pi
 
 
-def queryWKT(table, where_clause, cursor=cur_read ):
+def queryWKT (table, where_clause, cursor=cur_read ):
 
     select_statement = 'SELECT  geom.STAsText() FROM %s WHERE %s' % (table, where_clause)
     # print select_statement
@@ -208,7 +267,7 @@ def queryWKT(table, where_clause, cursor=cur_read ):
     return None
 
 
-def avg(values):
+def avg (values):
     sum1 = 0.0
     n=0
     for v in values:
@@ -299,38 +358,37 @@ class CornerDetector():
         four_largest_angles = heapq.nlargest(4, enumerate(_angles), key=lambda x: x[1])
         indices_of_four_largest_angles = [i[0] for i in four_largest_angles]
         four_points = [_points[i] for i in indices_of_four_largest_angles]
-        # for pnt in four_points: print pnt
+        # print four_points
         # define NE, SE, SW and NW corner
-        # pnt_centroid = Point(avg(pnt.x for pnt in four_points), avg(pnt.y for pnt in four_points))
-        pnt_centroid = self.poly.centroid
+        # pnt_centroid = self.poly.centroid
         # print 'pnt_centroid: %s' % pnt_centroid
 
-        north_points = [pnt for pnt in four_points if pnt.y >= pnt_centroid.y]
-        north_min_x = min(pnt.x for pnt in north_points)
-        north_max_x = max(pnt.x for x in north_points)
-        south_points = [pnt for pnt in four_points if pnt.y < pnt_centroid.y]
-        south_min_x = min(pnt.x for pnt in south_points)
-        south_max_x = max(pnt.x for pnt in south_points)
-        points = {}
+        # north_points = [pnt for pnt in four_points if pnt.y >= pnt_centroid.y]
+        # north_min_x = min(pnt.x for pnt in north_points)
+        # north_max_x = max(pnt.x for x in north_points)
+        # south_points = [pnt for pnt in four_points if pnt.y < pnt_centroid.y]
+        # south_min_x = min(pnt.x for pnt in south_points)
+        # south_max_x = max(pnt.x for pnt in south_points)
+        # points = {}
         # print 'north_points:', north_points
         # print 'south_points:', south_points
-        try:
-            points['NE'] = north_points[0] if north_points[0].x > north_points[1].x else north_points[1] # north-east point
-            points['NW'] = north_points[0] if north_points[0].x < north_points[1].x else north_points[1] # north-west point
-            points['SE'] = south_points[0] if south_points[0].x > south_points[1].x else south_points[1] # south-east point
-            points['SW'] = south_points[0] if south_points[0].x < south_points[1].x else south_points[1] # south-west point
-        except:
-            self.four_corners = None
-            return
+        # try:
+        #     points['NE'] = north_points[0] if north_points[0].x > north_points[1].x else north_points[1] # north-east point
+        #     points['NW'] = north_points[0] if north_points[0].x < north_points[1].x else north_points[1] # north-west point
+        #     points['SE'] = south_points[0] if south_points[0].x > south_points[1].x else south_points[1] # south-east point
+        #     points['SW'] = south_points[0] if south_points[0].x < south_points[1].x else south_points[1] # south-west point
+        # except:
+        #     self.four_corners = None
+        #     return
         # print 'points:', points
 
         # check if bearing NW -> NE is within the limits: 45 < x < 135
-        nw_ne_bearing = bearing_deg( points['NW'], points['NE'])
-        if not 45 < nw_ne_bearing < 135:
-            print 'corners switched.'
-            points['NE'], points['NW'], points['SW'], points['SE'] = points['NW'], points['SW'], points['SE'], points['NE']
+        # nw_ne_bearing = bearing_deg( points['NW'], points['NE'])
+        # if not 45 < nw_ne_bearing < 135:
+        #     print 'corners switched.'
+        #     points['NE'], points['NW'], points['SW'], points['SE'] = points['NW'], points['SW'], points['SE'], points['NE']
 
-        self.four_corners = points
+        self.four_corners = four_points   # it maybe that there are less than four points returned
 
     def get_four_corners(self):
         return self.four_corners
